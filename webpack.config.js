0e520df661dba167c55942875a42f133b1c747ce
Armani Ferrante <armaniferrante@gmail.com>
2019-03-22 17:45:01 -0700

    Get past some deps

--- webpack.config.js
+++ webpack.config.js
@@ -3,14 +3,19 @@ const webpack = require('webpack');
 const fs = require('fs');
 
 module.exports = {
+  optimization: {
+    minimize: false,
+	namedModules: true
+  },
   target: 'node',
   entry: './webpack_entry.js',
   output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'app.bundle.js'
+    path: __dirname,
+    library: 'Web3c',
+    filename: 'index.js'
   },
   externals: {
-    'web3-bzz': '{}',
+    'web3-bzz': 'react-native',
     'react-native': 'react-native'
   },
   module: {
@@ -28,8 +33,17 @@ module.exports = {
               "aliases": {
                 "crypto": "react-native-crypto",
                 "randombytes": "react-native-randombytes",
-                "path" : "path-browserify"
-              }
+                "path": "path-browserify",
+                "http": "@tradle/react-native-http",
+                "https": "https-browserify",
+				"stream": "stream-browserify",
+				"vm": "vm-browserify",
+				"tty": "tty-browserify",
+				"fs": "react-native-level-fs",
+				"net": "react-native-tcp",
+				"os": "react-native-os"
+              },
+			  "throwForNonStringLiteral": true
             }]
           ]
         }
@@ -42,7 +56,8 @@ module.exports = {
   devtool: 'source-map',
   resolve: {
     alias: {
-      "scrypt": path.resolve(__dirname, "node_modules/scrypt.js")
+      "scrypt": path.resolve(__dirname, "node_modules/scrypt.js"),
+	  "crypto": path.resolve(__dirname, "node_modules/react-native-crypto")
     }
   },
   plugins: [
